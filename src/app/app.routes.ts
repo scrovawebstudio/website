ff1127707392decda11f9bcf410780f7bb78475c
Sujit Gajare <84766755+Sujit-Gajare05@users.noreply.github.com>
2026-03-27 15:15:39 +0530

    about & service pages update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Aboutpage } from './components/aboutPage/aboutpage/aboutpage';
 import { Home } from './components/homePage/home/home';
+import { Servicepage } from './components/servicepage/servicepage';
 
 export const routes: Routes = [
     { path: '', component: Home, title: 'Home' },
     { path: 'about', component: Aboutpage, title: 'About Us' },
+    { path: 'services', component: Servicepage, title: 'Services' },
     { path: '**', redirectTo: '' }
 ];
---
+++ src/app/components/servicepage/servicepage.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Header } from '../header/header';
+import { ContactInfo } from '../contact-info/contact-info';
+import { Copyright } from '../copyright/copyright';
+
+@Component({
+  selector: 'app-servicepage',
+  imports: [],
+  templateUrl: './servicepage.html',
+  styleUrl: './servicepage.scss',
+})
+export class Servicepage {
+
+}
