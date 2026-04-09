39eaaf63ac926b6d8b050ff787158a34a55a5d78
Sujit Gajare <84766755+Sujit-Gajare05@users.noreply.github.com>
2026-04-09 22:38:02 +0530

    mobile responsive changes

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -8,5 +8,13 @@ import { RouterLink, RouterLinkActive } from "@angular/router";
   styleUrl: './header.scss',
 })
 export class Header {
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
 }
